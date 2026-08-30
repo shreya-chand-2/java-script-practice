367c9bb8d5ecfc5af07eee85d021c795f3c378a9
Shreya Chand <shreyachand2k8@gmail.com>
2026-08-30 08:32:40 +0000

    data types and conversions

---
+++ 01_basics/2-dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS CODE as newer version
+// alert(3+3) // we r using nodejs,not browser
+console.log(
+    3
+    +
+    3
+) //readability shud b there 
+let name ="shreya"
+let age =18
+let isLogged=true
+let state=null;
+//number->2 to power 53
+//bigint
+/*
+string=""
+boolean true/false
+null=>standalone value
+undefined
+symbol unique
+object
+*/
+console.log(typeof "String")
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("shreya")
+const a=100;
+let b="rftghj"
+var c="1234567876543"
+//u cant chnge a const
+/*
+prefer not to use var because of issue in block scope
+and functional scope
+*/
+console.table(["before values",a,b,c])
+b='2' 
+c='a'
+console.table(["after values",a,b,c])
